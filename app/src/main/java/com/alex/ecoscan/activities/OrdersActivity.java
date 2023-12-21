35c54a4608449f943cecd0b418037b83740eadcb
Alex Klim <91628959+alexlklim@users.noreply.github.com>
2023-12-21 13:10:06 +0100

    alex - menu hide synch orders work in OrdersActivity

--- app/src/main/java/com/alex/ecoscan/activities/OrdersActivity.java
+++ app/src/main/java/com/alex/ecoscan/activities/OrdersActivity.java
@@ -17,6 +17,9 @@ import com.alex.ecoscan.adapters.OrdersAdapter;
 import com.alex.ecoscan.database.RoomDB;
 import com.alex.ecoscan.managers.SettingsMng;
 import com.alex.ecoscan.managers.SynchMan;
+import com.alex.ecoscan.model.Order;
+
+import java.util.List;
 
 public class OrdersActivity extends AppCompatActivity implements OrdersAdapter.OnItemClickListener{
     private static final String TAG = "OrdersActivity";
@@ -25,6 +28,8 @@ public class OrdersActivity extends AppCompatActivity implements OrdersAdapter.O
     OrdersAdapter orderAdapter;
     SynchMan synchMan;
     RoomDB roomDB;
+
+    boolean showNotSynchOrders;
     SettingsMng settingsMng;
 
     @Override
@@ -36,6 +41,8 @@ public class OrdersActivity extends AppCompatActivity implements OrdersAdapter.O
         roomDB = RoomDB.getInstance(this);
         context = getApplicationContext();
         settingsMng = new SettingsMng(this);
+
+
 
         initializeRecyclerView();
 
@@ -57,7 +64,11 @@ public class OrdersActivity extends AppCompatActivity implements OrdersAdapter.O
 
     private void initializeRecyclerView() {
         recyclerView = findViewById(R.id.os_rv_orders);
-        orderAdapter = new OrdersAdapter(roomDB.orderDAO().getAll(), this, roomDB);
+
+        List<Order> orderList = roomDB.orderDAO().getAll();
+        if(showNotSynchOrders) orderList.removeIf(order -> order.getIsSynch() == 1);
+
+        orderAdapter = new OrdersAdapter(orderList, this, roomDB);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(orderAdapter);
@@ -79,14 +90,12 @@ public class OrdersActivity extends AppCompatActivity implements OrdersAdapter.O
         popupMenu.getMenuInflater().inflate(R.menu.menu_orders, popupMenu.getMenu());
         popupMenu.setOnMenuItemClickListener(item -> {
             int itemId = item.getItemId(); // Store the item ID in a final variable
-            if (itemId == R.id.menu_orders) {
+            if (itemId == R.id.menu_synchOrders) {
 //                synchWithServer();
                 return true;
-            } else if (itemId == R.id.menu_clearOrders) {
-//                synchMan.clearSynchOrders();
-                orderAdapter.notifyDataSetChanged();
-                initializeRecyclerView();
-                return true;
+            } else if (itemId == R.id.menu_hideOrders) {
+              showNotSynchOrders = true;
+              initializeRecyclerView();
             }
             return false;
         });
