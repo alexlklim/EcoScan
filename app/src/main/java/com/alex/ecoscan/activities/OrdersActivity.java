a7f1af8e433c5777c754d18287f04e9625b13b52
Alex Klim <91628959+alexlklim@users.noreply.github.com>
2024-01-03 11:14:13 +0100

    alex - complete menu for synchronization in OrdersActivity, it works properly

--- app/src/main/java/com/alex/ecoscan/activities/OrdersActivity.java
+++ app/src/main/java/com/alex/ecoscan/activities/OrdersActivity.java
@@ -60,7 +60,6 @@ public class OrdersActivity extends AppCompatActivity implements OrdersAdapter.O
 
         autoSynch();
 
-
     }
 
     private void autoSynch() {
@@ -115,14 +114,19 @@ public class OrdersActivity extends AppCompatActivity implements OrdersAdapter.O
         Log.i(TAG, "showPopupMenu: ");
         PopupMenu popupMenu = new PopupMenu(this, view);
         popupMenu.getMenuInflater().inflate(R.menu.menu_orders, popupMenu.getMenu());
+
+
         popupMenu.setOnMenuItemClickListener(item -> {
             int itemId = item.getItemId(); // Store the item ID in a final variable
             if (itemId == R.id.menu_synchOrders) {
-//                synchWithServer();
+                autoSynch();
                 return true;
             } else if (itemId == R.id.menu_hideOrders) {
               showNotSynchOrders = true;
               initializeRecyclerView();
+            } else if (itemId == R.id.menu_showOrders) {
+                showNotSynchOrders = false;
+                initializeRecyclerView();
             }
             return false;
         });
