bd6ffb44b7eeea910014b942c160d677ffe58d81
Alex Klim <91628959+alexlklim@users.noreply.github.com>
2024-01-03 08:01:42 +0100

    alex - add check network connection and check if server available

--- app/src/main/java/com/alex/ecoscan/interfaces/INetworkMng.java
+++ app/src/main/java/com/alex/ecoscan/interfaces/INetworkMng.java
@@ -1,9 +1,12 @@
 package com.alex.ecoscan.interfaces;
 
+import android.content.Context;
+
 public interface INetworkMng {
 
-    boolean isNetworkAvailable();
-    boolean isServerAvailable();
+    // Method to check if the device has an active internet connection
+    boolean isNetworkAvailable(Context context);
+    boolean isServerAvailable(String serverURL);
 
     boolean sendJsonData(String json);
 }
--- app/src/main/java/com/alex/ecoscan/managers/NetworkMng.java
+++ app/src/main/java/com/alex/ecoscan/managers/NetworkMng.java
@@ -1,27 +1,52 @@
 package com.alex.ecoscan.managers;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 import com.alex.ecoscan.interfaces.INetworkMng;
-import com.alex.ecoscan.model.utiles.Util;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
 public class NetworkMng implements INetworkMng {
     private static final String TAG = "NetworkMng";
 
     @Override
-    public boolean isNetworkAvailable() {
+    public boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+            return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+        }
         return false;
     }
 
     @Override
-    public boolean isServerAvailable() {
-        return false;
+    public boolean isServerAvailable(String serverURL) {
+        HttpURLConnection connection = null;
+
+        try {
+            URL url = new URL(serverURL);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000); // timeout in milliseconds
+
+            int responseCode = connection.getResponseCode();
+
+            // Check if the response code indicates success (2xx)
+            return responseCode >= 200 && responseCode < 300;
+
+        } catch (IOException e) {
+            // An error occurred (e.g., connection timeout)
+            return false;
+
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
     }
 
     @Override
